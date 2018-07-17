72146c7ce3d19d6f39e474b6b1f19871049e8874
liz <Hi_lize@163.com>
2018-07-17 23:48:55 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
      entry:'./src/index.js', //入口文件
      output:{
           //node.js中__dirname变量获取当前模块文件所在目录的完整绝对路径 
-          path:path.resolve(__dirname,'dist'), //输出位置
+          path:path.resolve(__dirname,'doc'), //输出位置
           filename:'main.js' //输入文件
      },
      /**
@@ -54,7 +54,7 @@ module.exports = {
         historyApiFallback: true,  //不跳转，在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，任意的 404 响应都可能需要被替代为 index.html
 　　　　inline: true,              //实时刷新
         // 设置服务器访问的基本目录
-        contentBase:path.resolve(__dirname,'dist'), //指定服务开启的位置，在dist文件夹中,该路径==='./dist'
+        contentBase:path.resolve(__dirname,'doc'), //指定服务开启的位置，在doc文件夹中,该路径==='./doc'
         // 设置服务器的ip地址,可以是localhost
         host:'localhost',
         // 设置端口
@@ -70,7 +70,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(), //调用webpack的热更新插件
         //利用webpack-html-plugin这个插件它可以生成html文件到指定的目录下，这样就可以不用再根目录下建立页面文件了，直接在src下建立模板文件，
         new HtmlWebpackPlugin({
-          template: __dirname + '/src/index.html'  //默认会在dist路径下生成index.html并引用所有的静态资源
+          template: __dirname + '/src/index.html'  //默认会在doc路径下生成index.html并引用所有的静态资源
         })
     ]
 }
